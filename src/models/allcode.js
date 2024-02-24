2255b59c6862c8c559a6e00da73828f5814d85c2
Duy <duyvu372001@gmail.com>
2024-02-24 10:08:13 +0900

    loadingData

--- src/models/allcode.js
+++ src/models/allcode.js
@@ -9,6 +9,10 @@ module.exports = (sequelize, DataTypes) => {
         as: 'positionData',
       })
       Allcode.hasMany(models.User, { foreignKey: 'gender', as: 'genderData' })
+      Allcode.hasMany(models.Schedule, {
+        foreignKey: 'timeType',
+        as: 'timeTypeData',
+      })
     }
   }
   Allcode.init(
