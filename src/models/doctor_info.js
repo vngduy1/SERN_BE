355445d084e76ac1bd78a856f7004c3411014f96
Duy <duyvu372001@gmail.com>
2024-02-24 11:32:12 +0900

    TableDoctorInfo

---
+++ src/models/doctor_info.js
@@ -0,0 +1,26 @@
+'use strict'
+const { Model } = require('sequelize')
+module.exports = (sequelize, DataTypes) => {
+  class Doctor_Info extends Model {
+    static associate(models) {
+      //Định danh các mối quan hệ
+    }
+  }
+  Doctor_Info.init(
+    {
+      doctorId: DataTypes.INTEGER,
+      priceId: DataTypes.STRING,
+      provinceId: DataTypes.STRING,
+      paymentId: DataTypes.STRING,
+      addressClinic: DataTypes.STRING,
+      nameClinic: DataTypes.STRING,
+      note: DataTypes.STRING,
+      count: DataTypes.INTEGER,
+    },
+    {
+      sequelize,
+      modelName: 'Doctor_Info',
+    },
+  )
+  return Doctor_Info
+}
